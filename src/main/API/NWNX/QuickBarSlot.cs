fb6e7a9960983e041c84d5b4e91c89cfd0a4947e
Jorteck <jorteckdev@gmail.com>
2020-03-07 00:10:57 +0100

    API - Add NWNX.Player plugin interop.

---
+++ src/main/API/NWNX/QuickBarSlot.cs
@@ -0,0 +1,19 @@
+namespace NWM.API.NWNX
+{
+  public struct QuickBarSlot
+  {
+    public NwObject Item { get; set; }
+    public NwObject SecondaryItem { get; set; }
+    public QuickBarSlotType ObjectType { get; set; }
+    public int MultiClass { get; set; }
+    public string Resref { get; set; }
+    public string CommandLabel { get; set; }
+    public string CommandLine { get; set; }
+    public string ToolTip { get; set; }
+    public int INTParam1 { get; set; }
+    public int MetaType { get; set; }
+    public int DomainLevel { get; set; }
+    public int AssociateType { get; set; }
+    public NwObject Associate { get; set; }
+  }
+}
