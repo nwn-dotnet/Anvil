db0d6fedd8e2b3f2229cf2227b4a1fd64ab4218e
Jorteck <jorteckdev@gmail.com>
2020-04-05 01:14:41 +0200

    Add constants.

---
+++ src/main/API/Constants/Native/ItemProperty/IPSaveBaseType.cs
@@ -0,0 +1,11 @@
+using NWN;
+
+namespace NWM.API.Constants
+{
+  public enum IPSaveBaseType
+  {
+    Fortitude = NWScript.IP_CONST_SAVEBASETYPE_FORTITUDE,
+    Will = NWScript.IP_CONST_SAVEBASETYPE_WILL,
+    Reflex = NWScript.IP_CONST_SAVEBASETYPE_REFLEX
+  }
+}
