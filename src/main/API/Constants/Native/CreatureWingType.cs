1f5c7d874037ea1973e4a9c8b5f7c0df3f639d4b
Jorteck <jorteckdev@gmail.com>
2020-04-05 01:51:58 +0200

    Add Constants.

---
+++ src/main/API/Constants/Native/CreatureWingType.cs
@@ -0,0 +1,15 @@
+using NWN;
+
+namespace NWM.API.Constants
+{
+  public enum CreatureWingType
+  {
+    None = NWScript.CREATURE_WING_TYPE_NONE,
+    Demon = NWScript.CREATURE_WING_TYPE_DEMON,
+    Angel = NWScript.CREATURE_WING_TYPE_ANGEL,
+    Bat = NWScript.CREATURE_WING_TYPE_BAT,
+    Dragon = NWScript.CREATURE_WING_TYPE_DRAGON,
+    Butterfly = NWScript.CREATURE_WING_TYPE_BUTTERFLY,
+    Bird = NWScript.CREATURE_WING_TYPE_BIRD
+  }
+}
