56312f1c4587c40d23120172b1245c1ffcbf7ae5
Jorteck <jorteckdev@gmail.com>
2020-04-05 02:02:33 +0200

    Finish IP constant enum mapping.

---
+++ src/main/API/Constants/Native/ItemProperty/IPTrapStrength.cs
@@ -0,0 +1,12 @@
+using NWN;
+
+namespace NWM.API.Constants
+{
+  public enum IPTrapStrength
+  {
+    Minor = NWScript.IP_CONST_TRAPSTRENGTH_MINOR,
+    Average = NWScript.IP_CONST_TRAPSTRENGTH_AVERAGE,
+    Strong = NWScript.IP_CONST_TRAPSTRENGTH_STRONG,
+    Deadly = NWScript.IP_CONST_TRAPSTRENGTH_DEADLY,
+  }
+}
