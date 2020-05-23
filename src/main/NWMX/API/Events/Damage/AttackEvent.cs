cb99ca1d383ed3f7aedc1563bdd542c29da3a8dc
Jorteck <jorteckdev@gmail.com>
2020-05-23 09:31:44 +0200

    Attack/Damage events - Add attacker and target values.

--- src/main/NWMX/API/Events/Damage/AttackEvent.cs
+++ src/main/NWMX/API/Events/Damage/AttackEvent.cs
@@ -8,12 +8,18 @@ namespace NWMX.API.Events
   [NWNXAttackEvent]
   public sealed class AttackEvent : IEvent<AttackEvent>
   {
-    public AttackEventData Data;
+    public NwObject Attacker { get; private set; }
+    public NwObject Target { get; private set; }
+    public AttackEventData Data { get; set; }
 
     public void BroadcastEvent(NwObject objSelf)
     {
       Data = DamagePlugin.GetAttackEventData();
+      Attacker = Data.oTarget.ToNwObject();
+      Target = objSelf;
+
       Callbacks?.Invoke(this);
+
       DamagePlugin.SetAttackEventData(Data);
     }
 
--- src/main/NWMX/API/Events/Damage/DamageEvent.cs
+++ src/main/NWMX/API/Events/Damage/DamageEvent.cs
@@ -8,12 +8,18 @@ namespace NWMX.API.Events
   [NWNXDamageEvent]
   public sealed class DamageEvent : IEvent<DamageEvent>
   {
-    public DamageEventData Data;
+    public NwObject Damager { get; private set; }
+    public NwObject Target { get; private set; }
+    public DamageEventData Data { get; set; }
 
     public void BroadcastEvent(NwObject objSelf)
     {
       Data = DamagePlugin.GetDamageEventData();
+      Damager = Data.oDamager.ToNwObject();
+      Target = objSelf;
+
       Callbacks?.Invoke(this);
+
       DamagePlugin.SetDamageEventData(Data);
     }
 
