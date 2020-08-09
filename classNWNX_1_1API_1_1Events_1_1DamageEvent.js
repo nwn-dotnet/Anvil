b5bdb993aba35ac1520106e3d9d0151be578e122
Jorteck <Jorteck@users.noreply.github.com>
2020-08-09 17:35:48 +0000

    Automated Commit: Update docs 9454e3d63405e24d101a22771e01994e5a421ed2

--- classNWNX_1_1API_1_1Events_1_1DamageEvent.js
+++ classNWNX_1_1API_1_1Events_1_1DamageEvent.js
@@ -1,8 +1,8 @@
 var classNWNX_1_1API_1_1Events_1_1DamageEvent =
 [
+    [ "InvokeCallbacks", "classNWNX_1_1API_1_1Events_1_1DamageEvent.html#aee516c5b3352a965674dd6623e3a04f3", null ],
     [ "PrepareEvent", "classNWNX_1_1API_1_1Events_1_1DamageEvent.html#a03d086fc9ac3cb710259bb6d6f508923", null ],
     [ "Attacker", "classNWNX_1_1API_1_1Events_1_1DamageEvent.html#a0b1788979ea5ca927caf6ee70c207c3d", null ],
     [ "DamageData", "classNWNX_1_1API_1_1Events_1_1DamageEvent.html#aab3cb8e74cae662660ff73ca4d9445f1", null ],
-    [ "Target", "classNWNX_1_1API_1_1Events_1_1DamageEvent.html#a076ea9eaccc3c663329868a2934bfbad", null ],
-    [ "Callbacks", "classNWNX_1_1API_1_1Events_1_1DamageEvent.html#a49a0d5206124644e9b49d898f8b572b4", null ]
+    [ "Target", "classNWNX_1_1API_1_1Events_1_1DamageEvent.html#a076ea9eaccc3c663329868a2934bfbad", null ]
 ];
