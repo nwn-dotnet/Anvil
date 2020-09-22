7108a6a2c6be3a4615bd33ff59329c577bbae519
Jorteck <jorteckdev@gmail.com>
2020-09-22 23:52:41 +0200

    Hide bootstrap method from docs.

--- src/main/NWN/Bootstrap.cs
+++ src/main/NWN/Bootstrap.cs
@@ -3,10 +3,12 @@ using JetBrains.Annotations;
 
 namespace NWN
 {
+  /// @cond INTERNAL
   public static class Internal
   {
     [UsedImplicitly] // Called by NWNX
     public static int Bootstrap(IntPtr arg, int argLength)
       => NManager.Init(arg, argLength);
   }
+  // @endcond
 }
