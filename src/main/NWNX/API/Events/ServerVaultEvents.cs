0ae2202abb4ed2c3f94f2a2134c0d5bb09568e16
nate <nate@pop-os.localdomain>
2020-09-22 14:54:40 -0500

    updated server vault events

--- src/main/NWNX/API/Events/ServerVaultEvents.cs
+++ src/main/NWNX/API/Events/ServerVaultEvents.cs
@@ -1,18 +1,30 @@
 using NWN.API;
+using NWN.API.Events;
 
 namespace NWNX.API.Events
 {
-  public static class ServerVaultEvents
-  {
-    [NWNXEvent("NWNX_ON_SERVER_CHARACTER_SAVE_BEFORE")]
-    public class OnServerCharacterSaveBefore : EventSkippable<OnServerCharacterSaveBefore>
+    public static class ServerVaultEvents
     {
-      public NwPlayer Player { get; private set; }
+        [NWNXEvent("NWNX_ON_SERVER_CHARACTER_SAVE_BEFORE")]
+        public class OnServerCharacterSaveBefore : EventSkippable<OnServerCharacterSaveBefore>
+        {
+            public NwPlayer Player { get; private set; }
 
-      protected override void PrepareEvent(NwObject objSelf)
-      {
-        Player = (NwPlayer) objSelf;
-      }
+            protected override void PrepareEvent(NwObject objSelf)
+            {
+                Player = (NwPlayer)objSelf;
+            }
+        }
+
+        [NWNXEvent("NWNX_ON_SERVER_CHARACTER_SAVE_AFTER")]
+        public class OnServerCharacterSaveAfter : Event<OnServerCharacterSaveAfter>
+        {
+            public NwPlayer Player { get; private set; }
+
+            protected override void PrepareEvent(NwObject objSelf)
+            {
+                Player = (NwPlayer)objSelf;
+            }
+        }
     }
-  }
 }
