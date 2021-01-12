d06affec0c52447fa7225978e27f1a99983b1a1e
Philip Milliorn <scottmilliorn@outlook.com>
2021-01-11 19:23:14 -0800

    format

--- src/main/NWNX/API/Weapon.cs
+++ src/main/NWNX/API/Weapon.cs
@@ -4,18 +4,18 @@ using NWN.Core.NWNX;
 
 namespace NWNX.API
 {
-    // TODO implement plugin functions
-    public static class Weapon
+  // TODO implement plugin functions
+  public static class Weapon
+  {
+    static Weapon()
     {
-        static Weapon()
-        {
-            PluginUtils.AssertPluginExists<WeaponPlugin>();
-        }
-
-        /// <summary>
-        ///  Set baseItem to be a Monk weapon.
-        /// </summary>
-        /// <param name="baseItem">BaseItemType for monk weapon.</param>
-        public static void SetWeaponIsMonkWeapon(this BaseItemType baseItem) => WeaponPlugin.SetWeaponIsMonkWeapon((int)baseItem);
+      PluginUtils.AssertPluginExists<WeaponPlugin>();
     }
+
+    /// <summary>
+    /// Set baseItem to be a Monk weapon.
+    /// </summary>
+    /// <param name="baseItem">BaseItemType for monk weapon.</param>
+    public static void SetWeaponIsMonkWeapon(this BaseItemType baseItem) => WeaponPlugin.SetWeaponIsMonkWeapon((int)baseItem);
+  }
 }
