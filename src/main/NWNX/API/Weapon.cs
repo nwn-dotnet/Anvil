09714c3b8392c0eb2f0e4bde9bd18185e7801415
Philip Milliorn <scottmilliorn@outlook.com>
2021-01-11 19:16:07 -0800

    SetWeaponIsMonkWeapon

--- src/main/NWNX/API/Weapon.cs
+++ src/main/NWNX/API/Weapon.cs
@@ -1,13 +1,21 @@
+using NWN.API;
+using NWN.API.Constants;
 using NWN.Core.NWNX;
 
 namespace NWNX.API
 {
-  // TODO implement plugin functions
-  public static class Weapon
-  {
-    static Weapon()
+    // TODO implement plugin functions
+    public static class Weapon
     {
-      PluginUtils.AssertPluginExists<WeaponPlugin>();
+        static Weapon()
+        {
+            PluginUtils.AssertPluginExists<WeaponPlugin>();
+        }
+
+        /// <summary>
+        ///  Set baseItem to be a Monk weapon.
+        /// </summary>
+        /// <param name="baseItem">BaseItemType for monk weapon.</param>
+        public static void SetWeaponIsMonkWeapon(this BaseItemType baseItem) => WeaponPlugin.SetWeaponIsMonkWeapon((int)baseItem);
     }
-  }
 }
