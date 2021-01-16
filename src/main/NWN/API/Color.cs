45f61cfab613f54f51d3858230c17f424324cc1e
Philip Milliorn <scottmilliorn@outlook.com>
2021-01-15 16:30:54 -0800

    ORANGE

--- src/main/NWN/API/Color.cs
+++ src/main/NWN/API/Color.cs
@@ -26,7 +26,7 @@ namespace NWN.API
     public static readonly Color PURPLE = new (128, 0, 128);
     public static readonly Color TEAL = new (0, 128, 128);
     public static readonly Color NAVY = new (0, 0, 128);
-
+    public static readonly Color ORANGE = new (255, 165, 0);
 
     /// <summary>
     /// Gets the red value of this color as a byte (0-255).
