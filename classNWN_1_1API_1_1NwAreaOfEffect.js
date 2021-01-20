763b877a242706ad69c3df53d3e62bda4eb8c2e3
Jorteck <Jorteck@users.noreply.github.com>
2021-01-20 18:05:06 +0000

    Automated Commit: Update docs 4c498a61f3b6c0a039f19f1b914faace7617e089

--- classNWN_1_1API_1_1NwAreaOfEffect.js
+++ classNWN_1_1API_1_1NwAreaOfEffect.js
@@ -1,6 +1,6 @@
 var classNWN_1_1API_1_1NwAreaOfEffect =
 [
-    [ "GetObjectsInEffectArea", "classNWN_1_1API_1_1NwAreaOfEffect.html#a7ecb1cd589ebb2039e1af65c5cc913ca", null ],
-    [ "GetObjectsInEffectArea< T >", "classNWN_1_1API_1_1NwAreaOfEffect.html#a2f3c612485c09a5a9e30b5a99741a60f", null ],
+    [ "GetObjectsInEffectArea", "classNWN_1_1API_1_1NwAreaOfEffect.html#ad42e343cf28ffdd27cae99660e7e1fd5", null ],
+    [ "GetObjectsInEffectArea< T >", "classNWN_1_1API_1_1NwAreaOfEffect.html#affb454bd0dc09731809f951c0a35021d", null ],
     [ "Creator", "classNWN_1_1API_1_1NwAreaOfEffect.html#a08591903aebfa2d1da48be35971186bb", null ]
 ];
