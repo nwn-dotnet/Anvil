c69f44323ee95028f7d37f19328917d60112b352
Jorteck <jorteckdev@gmail.com>
2021-02-13 01:48:29 +0100

    Fix enum spacing.

--- src/main/NWN/Services/Services/Bindings/BindingType.cs
+++ src/main/NWN/Services/Services/Bindings/BindingType.cs
@@ -6,6 +6,7 @@ namespace NWN.Services
     /// Creates and starts a single instance of the service during startup and uses the same instance when injected to other services.
     /// </summary>
     Singleton,
+
     /// <summary>
     /// Creates and starts a new instance of the service for each place the service is injected.
     /// </summary>
