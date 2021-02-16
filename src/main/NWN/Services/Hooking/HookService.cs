a74525c1681a680a06000fc0b6e8a6cc568fe2a5
Jorteck <jorteckdev@gmail.com>
2021-02-16 21:00:30 +0100

    Update hook order constants.

--- src/main/NWN/Services/Hooking/HookService.cs
+++ src/main/NWN/Services/Hooking/HookService.cs
@@ -22,14 +22,14 @@ namespace NWN.Services
     /// </summary>
     /// <param name="address">The address of the native function. Use the constants available in the NWN.Native library:<see cref="NWN.Native.API.NWNXLib.Functions"/>.</param>
     /// <param name="handler">The handler to be invoked when this function is called. Once hooked, the original function will not be called, and must be invoked manually via the returned object.</param>
-    /// <param name="priority">The execution order for this hook. See the constants in <see cref="HookPriority"/>.</param>
+    /// <param name="order">The execution order for this hook. See the constants in <see cref="HookOrder"/>.</param>
     /// <typeparam name="T">The delegate type that identically matches the native function signature.</typeparam>
     /// <returns>A wrapper object containing a delegate to the original function. The wrapped object can be disposed to release the hook.</returns>
-    public FunctionHook<T> RequestHook<T>(uint address, T handler, int priority = HookPriority.Default) where T : Delegate
+    public FunctionHook<T> RequestHook<T>(uint address, T handler, int order = HookOrder.Default) where T : Delegate
     {
       Log.Debug($"Requesting function hook for {typeof(T).Name}, address 0x{address:X}");
       IntPtr managedFuncPtr = Marshal.GetFunctionPointerForDelegate(handler);
-      IntPtr nativeFuncPtr = VM.RequestHook(new IntPtr(address), managedFuncPtr, priority);
+      IntPtr nativeFuncPtr = VM.RequestHook(new IntPtr(address), managedFuncPtr, order);
 
       FunctionHook<T> hook = new FunctionHook<T>(this, nativeFuncPtr);
       hooks.Add(hook);
