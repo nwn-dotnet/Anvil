0767b2848e6b06f032c4f2f4ca5834b95f0cb277
Urothis <natecaple@gmail.com>
2021-02-21 18:27:18 -0600

    Update to dialog plugin

--- src/main/NWNX/API/Dialog.cs
+++ src/main/NWNX/API/Dialog.cs
@@ -1,4 +1,6 @@
+using NWN.API;
 using NWN.Core.NWNX;
+using NWNX.API.Constants;
 
 namespace NWNX.API
 {
@@ -15,5 +17,53 @@ namespace NWNX.API
       get => DialogPlugin.GetCurrentNodeText();
       set => DialogPlugin.SetCurrentNodeText(value);
     }
+
+    public static NodeType GetCurrentNodeType() =>
+        (NodeType) DialogPlugin.GetCurrentNodeType();
+
+    public static ScriptType GetCurrentScriptType() =>
+        (ScriptType)DialogPlugin.GetCurrentScriptType();
+
+    public static int GetCurrentNodeID() =>
+        DialogPlugin.GetCurrentNodeID();
+
+    public static int GetCurrentNodeIndex() =>
+        DialogPlugin.GetCurrentNodeIndex();
+
+    public static void End(this NwCreature creature) =>
+        DialogPlugin.End(creature);
   }
+}
+
+/* 
+int NWNX_Dialog_GetCurrentNodeType()
+{
+    string sFunc = "GetCurrentNodeType";
+
+    NWNX_CallFunction(NWNX_Dialog, sFunc);
+    return NWNX_GetReturnValueInt(NWNX_Dialog, sFunc);
+}
+
+int NWNX_Dialog_GetCurrentScriptType()
+{
+    string sFunc = "GetCurrentScriptType";
+
+    NWNX_CallFunction(NWNX_Dialog, sFunc);
+    return NWNX_GetReturnValueInt(NWNX_Dialog, sFunc);
+}
+
+int NWNX_Dialog_GetCurrentNodeID()
+{
+    string sFunc = "GetCurrentNodeID";
+
+    NWNX_CallFunction(NWNX_Dialog, sFunc);
+    return NWNX_GetReturnValueInt(NWNX_Dialog, sFunc);
+}
+
+int NWNX_Dialog_GetCurrentNodeIndex()
+{
+    string sFunc = "GetCurrentNodeIndex";
+
+    NWNX_CallFunction(NWNX_Dialog, sFunc);
+    return NWNX_GetReturnValueInt(NWNX_Dialog, sFunc);
 }
