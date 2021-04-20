1517dd97312c6d990b37f4d66a46c57e71a6e449
Scott Milliorn <scottmilliorn@gmail.com>
2021-04-19 22:27:22 -0700

    AreaOnExit, AreaOnEnter

--- src/main/NWN/API/Events/Game/AreaEvents.cs
+++ src/main/NWN/API/Events/Game/AreaEvents.cs
@@ -16,12 +16,12 @@ namespace NWN.API.Events
     public sealed class OnEnter : IEvent
     {
       /// <summary>
-      /// Gets the area that was entered.
+      /// Gets the <see cref="NwArea"/> that was entered.
       /// </summary>
       public NwArea Area { get; } = NWScript.OBJECT_SELF.ToNwObject<NwArea>();
 
       /// <summary>
-      /// Gets the game object that entered the area.
+      /// Gets the <see cref="NwGameObject"/> that entered the <see cref="NwArea"/>.
       /// </summary>
       public NwGameObject EnteringObject { get; } = NWScript.GetEnteringObject().ToNwObject<NwGameObject>();
 
@@ -29,18 +29,18 @@ namespace NWN.API.Events
     }
 
     /// <summary>
-    /// Called when an object leaves the area.
+    /// Called when an <see cref="NwGameObject"/> leaves the <see cref="NwArea"/>.
     /// </summary>
     [GameEvent(EventScriptType.AreaOnExit)]
     public sealed class OnExit : IEvent
     {
       /// <summary>
-      /// Gets the area that was left.
+      /// Gets the <see cref="NwArea"/> that was left.
       /// </summary>
       public NwArea Area { get; } = NWScript.OBJECT_SELF.ToNwObject<NwArea>();
 
       /// <summary>
-      /// Gets the game object that left the area.
+      /// Gets the <see cref="NwGameObject"/> that left the <see cref="NwArea"/>.
       /// </summary>
       public NwGameObject ExitingObject { get; } = NWScript.GetExitingObject().ToNwObject<NwGameObject>();
 
