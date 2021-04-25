2f1926c131bab42d7e11967c2bc0f0f65191e13e
Jorteck <jorteckdev@gmail.com>
2021-04-25 23:38:29 +0200

    Add "IsDisconnectingPlayer" To Store OnClose event.

--- src/main/NWN/API/Events/Game/StoreEvents.cs
+++ src/main/NWN/API/Events/Game/StoreEvents.cs
@@ -34,9 +34,27 @@ namespace NWN.API.Events
       public NwStore Store { get; } = NWScript.OBJECT_SELF.ToNwObject<NwStore>();
 
       /// <summary>
-      /// Gets the <see cref="NwPlayer"/> that last closed this <see cref="NwStore"/>.
+      /// Gets the <see cref="NwCreature"/> that last closed this <see cref="NwStore"/>.<br/>
+      /// If this is a disconnecting player, this value will be a <see cref="NwCreature"/>. See <see cref="IsDisconnectingPlayer"/> to determine this state.
       /// </summary>
-      public NwPlayer Player { get; } = NWScript.GetLastClosedBy().ToNwObject<NwPlayer>();
+      public NwCreature Creature { get; } = NWScript.GetLastClosedBy().ToNwObject<NwCreature>();
+
+      /// <summary>
+      /// Gets a value indicating whether the <see cref="Creature"/> is a player leaving the server.
+      /// </summary>
+      public bool IsDisconnectingPlayer
+      {
+        get
+        {
+          if (Creature is not NwPlayer)
+          {
+            string objectId = Creature.ToString();
+            return objectId.Length == 8 && objectId.StartsWith("7ff");
+          }
+
+          return false;
+        }
+      }
 
       NwObject IEvent.Context => Store;
     }
