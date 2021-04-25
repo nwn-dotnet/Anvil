a14934b6baf61321ab1f5b1d8c78d1d3e5cdfc07
Scott Milliorn <scottmilliorn@gmail.com>
2021-04-24 20:10:40 -0700

    Encounter events (#227)
    * EncounterOnObjectEnter
    
    * EncounterOnObjectExit
    
    * corrections
    
    * refactor
    
    * EncounterOnEncounterExhausted
    
    * EncounterOnUserDefinedEvent
    
    * trim whitespace
    
    * Update src/main/NWN/API/Events/Game/EncounterEvents.cs
    
    Co-authored-by: Jorteck <jorteckdev@gmail.com>
    
    * Update src/main/NWN/API/Events/Game/EncounterEvents.cs
    
    Co-authored-by: Jorteck <jorteckdev@gmail.com>
    
    * Update src/main/NWN/API/Events/Game/EncounterEvents.cs
    
    Co-authored-by: Jorteck <jorteckdev@gmail.com>
    
    * Update src/main/NWN/API/Events/Game/EncounterEvents.cs
    
    Co-authored-by: Jorteck <jorteckdev@gmail.com>
    
    * Update src/main/NWN/API/Events/Game/EncounterEvents.cs
    
    Co-authored-by: Jorteck <jorteckdev@gmail.com>
    
    Co-authored-by: Jorteck <jorteckdev@gmail.com>

--- src/main/NWN/API/Events/Game/EncounterEvents.cs
+++ src/main/NWN/API/Events/Game/EncounterEvents.cs
@@ -5,13 +5,16 @@ using NWN.Core;
 namespace NWN.API.Events
 {
   /// <summary>
-  /// Events for Encounter triggers.
+  /// Events for <see cref="NwEncounter"/> triggers.
   /// </summary>
   public static class EncounterEvents
   {
     [GameEvent(EventScriptType.EncounterOnObjectEnter)]
     public sealed class OnEnter : IEvent
     {
+      /// <summary>
+      /// Gets the <see cref="NwEncounter"/> that was entered.
+      /// </summary>
       public NwEncounter Encounter { get; } = NWScript.OBJECT_SELF.ToNwObject<NwEncounter>();
 
       NwObject IEvent.Context => Encounter;
@@ -20,6 +23,9 @@ namespace NWN.API.Events
     [GameEvent(EventScriptType.EncounterOnObjectExit)]
     public sealed class OnExit : IEvent
     {
+      /// <summary>
+      /// Gets the <see cref="NwEncounter"/> that was exited.
+      /// </summary>
       public NwEncounter Encounter { get; } = NWScript.OBJECT_SELF.ToNwObject<NwEncounter>();
 
       NwObject IEvent.Context => Encounter;
@@ -28,6 +34,9 @@ namespace NWN.API.Events
     [GameEvent(EventScriptType.EncounterOnHeartbeat)]
     public sealed class OnHeartbeat : IEvent
     {
+      /// <summary>
+      /// Gets the <see cref="NwEncounter"/> associated with this heartbeat event.
+      /// </summary>
       public NwEncounter Encounter { get; } = NWScript.OBJECT_SELF.ToNwObject<NwEncounter>();
 
       NwObject IEvent.Context => Encounter;
@@ -36,6 +45,9 @@ namespace NWN.API.Events
     [GameEvent(EventScriptType.EncounterOnEncounterExhausted)]
     public sealed class OnExhausted : IEvent
     {
+      /// <summary>
+      /// Gets the <see cref="NwEncounter"/> that was exhausted.
+      /// </summary>
       public NwEncounter Encounter { get; } = NWScript.OBJECT_SELF.ToNwObject<NwEncounter>();
 
       NwObject IEvent.Context => Encounter;
@@ -44,6 +56,9 @@ namespace NWN.API.Events
     [GameEvent(EventScriptType.EncounterOnUserDefinedEvent)]
     public sealed class OnUserDefined : IEvent
     {
+      /// <summary>
+      /// Gets the <see cref="NwEncounter"/> associated with this user defined event.
+      /// </summary>
       public NwEncounter Encounter { get; } = NWScript.OBJECT_SELF.ToNwObject<NwEncounter>();
 
       NwObject IEvent.Context => Encounter;
