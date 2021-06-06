8e6c90f42a5103ed5178a4c370325af5e2f047b2
Jorteck <jorteckdev@gmail.com>
2021-06-06 22:59:00 +0200

    Extend IsPlayerControlled/IsLoginPlayerCharacter checks to NwObject.

--- src/main/NWN/API/Extensions/ObjectExtensions.cs
+++ src/main/NWN/API/Extensions/ObjectExtensions.cs
@@ -5,24 +5,24 @@ namespace NWN.API
   public static class ObjectExtensions
   {
     /// <summary>
-    /// Gets if this creature is currently being controlled by a player/DM.<br/>
+    /// Gets if this object is currently being controlled by a player/DM.<br/>
     /// If this creature is a possessed familiar or is DM possessed, this will return true.<br/>
     /// If this creature is a player creature (the creature a played logged in with), but the player is possessing another creature, this returns false.<br/>
     /// If no player is controlling this creature, this returns false.
     /// </summary>
-    public static bool IsPlayerControlled([CanBeNull] this NwCreature creature, out NwPlayer player)
+    public static bool IsPlayerControlled([CanBeNull] this NwObject gameObject, out NwPlayer player)
     {
-      player = creature?.ControllingPlayer;
+      player = gameObject is NwCreature creature ? creature.ControllingPlayer : null;
       return player != null;
     }
 
     /// <summary>
-    /// Gets if this creature is a player character/DM avatar.<br/>
+    /// Gets if this object is a player character/DM avatar.<br/>
     /// If this creature is a NPC or familiar, regardless of possession, this will return false.
     /// </summary>
-    public static bool IsLoginPlayerCharacter([CanBeNull] this NwCreature creature, out NwPlayer player)
+    public static bool IsLoginPlayerCharacter([CanBeNull] this NwObject gameObject, out NwPlayer player)
     {
-      player = creature?.LoginPlayer;
+      player = gameObject is NwCreature creature ? creature.LoginPlayer : null;
       return player != null;
     }
   }
