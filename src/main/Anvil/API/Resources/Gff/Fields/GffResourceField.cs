fa4a2747e4854efcd74ab3f223bda4b51cf53f04
Jorteck <jorteckdev@gmail.com>
2021-11-02 02:47:56 +0100

    Implement value conversions/getters.

--- src/main/Anvil/API/Resources/Gff/Fields/GffResourceField.cs
+++ src/main/Anvil/API/Resources/Gff/Fields/GffResourceField.cs
@@ -29,19 +29,207 @@ namespace Anvil.API
       get => throw new InvalidOperationException($"Cannot get child value of {FieldType} with field index.");
     }
 
-    protected virtual string ReadCExoString()
-    {
-      throw new InvalidOperationException($"Cannot convert {FieldType} to string.");
-    }
-
-    public static implicit operator string(GffResourceField field)
-    {
-      return field.ReadCExoString();
-    }
-
     public override string ToString()
     {
-      return ReadCExoString();
+      return Value().ToString();
+    }
+
+    public object Value()
+    {
+      switch (FieldType)
+      {
+        case GffResourceFieldType.Byte when TryReadByte(out byte value):
+          return value;
+        case GffResourceFieldType.Char when TryReadChar(out byte value):
+          return value;
+        case GffResourceFieldType.Word when TryReadWord(out ushort value):
+          return value;
+        case GffResourceFieldType.Short when TryReadShort(out short value):
+          return value;
+        case GffResourceFieldType.DWord when TryReadDWord(out uint value):
+          return value;
+        case GffResourceFieldType.Int when TryReadInt(out int value):
+          return value;
+        case GffResourceFieldType.DWord64 when TryReadDWord64(out ulong value):
+          return value;
+        case GffResourceFieldType.Int64 when TryReadInt64(out long value):
+          return value;
+        case GffResourceFieldType.Float when TryReadFloat(out float value):
+          return value;
+        case GffResourceFieldType.Double when TryReadDouble(out double value):
+          return value;
+        case GffResourceFieldType.CExoString when TryReadCExoString(out string value):
+          return value;
+        case GffResourceFieldType.CResRef when TryReadCResRef(out string value):
+          return value;
+        case GffResourceFieldType.CExoLocString when TryReadCExoLocString(out string value):
+          return value;
+        default:
+          throw new InvalidOperationException($"Cannot convert {FieldType} to value.");
+      }
+    }
+
+    public T Value<T>()
+    {
+      switch (FieldType)
+      {
+        case GffResourceFieldType.Byte when typeof(T) == typeof(byte) && TryReadByte(out byte value):
+          return (T)(object)value;
+        case GffResourceFieldType.Char when typeof(T) == typeof(byte) && TryReadChar(out byte value):
+          return (T)(object)value;
+        case GffResourceFieldType.Word when typeof(T) == typeof(ushort) && TryReadWord(out ushort value):
+          return (T)(object)value;
+        case GffResourceFieldType.Short when typeof(T) == typeof(short) && TryReadShort(out short value):
+          return (T)(object)value;
+        case GffResourceFieldType.DWord when typeof(T) == typeof(uint) && TryReadDWord(out uint value):
+          return (T)(object)value;
+        case GffResourceFieldType.Int when typeof(T) == typeof(int) && TryReadInt(out int value):
+          return (T)(object)value;
+        case GffResourceFieldType.DWord64 when typeof(T) == typeof(ulong) && TryReadDWord64(out ulong value):
+          return (T)(object)value;
+        case GffResourceFieldType.Int64 when typeof(T) == typeof(long) && TryReadInt64(out long value):
+          return (T)(object)value;
+        case GffResourceFieldType.Float when typeof(T) == typeof(float) && TryReadFloat(out float value):
+          return (T)(object)value;
+        case GffResourceFieldType.Double when typeof(T) == typeof(double) && TryReadDouble(out double value):
+          return (T)(object)value;
+        case GffResourceFieldType.CExoString when typeof(T) == typeof(string) && TryReadCExoString(out string value):
+          return (T)(object)value;
+        case GffResourceFieldType.CResRef when typeof(T) == typeof(string) && TryReadCResRef(out string value):
+          return (T)(object)value;
+        case GffResourceFieldType.CExoLocString when typeof(T) == typeof(string) && TryReadCExoLocString(out string value):
+          return (T)(object)value;
+        default:
+          throw new InvalidOperationException($"Cannot convert {FieldType} to {typeof(T).Name}");
+      }
+    }
+
+    public static explicit operator byte(GffResourceField field)
+    {
+      return field.Value<byte>();
+    }
+
+    public static explicit operator ushort(GffResourceField field)
+    {
+      return field.Value<ushort>();
+    }
+
+    public static explicit operator short(GffResourceField field)
+    {
+      return field.Value<short>();
+    }
+
+    public static explicit operator uint(GffResourceField field)
+    {
+      return field.Value<uint>();
+    }
+
+    public static explicit operator int(GffResourceField field)
+    {
+      return field.Value<int>();
+    }
+
+    public static explicit operator ulong(GffResourceField field)
+    {
+      return field.Value<ulong>();
+    }
+
+    public static explicit operator long(GffResourceField field)
+    {
+      return field.Value<long>();
+    }
+
+    public static explicit operator float(GffResourceField field)
+    {
+      return field.Value<float>();
+    }
+
+    public static explicit operator double(GffResourceField field)
+    {
+      return field.Value<double>();
+    }
+
+    public static explicit operator string(GffResourceField field)
+    {
+      return field.Value<string>();
+    }
+
+    public virtual bool TryReadCExoString(out string value)
+    {
+      value = default;
+      return false;
+    }
+
+    public virtual bool TryReadCResRef(out string value)
+    {
+      value = default;
+      return false;
+    }
+
+    public virtual bool TryReadInt(out int value)
+    {
+      value = default;
+      return false;
+    }
+
+    public virtual bool TryReadInt64(out long value)
+    {
+      value = default;
+      return false;
+    }
+
+    public virtual bool TryReadByte(out byte value)
+    {
+      value = default;
+      return false;
+    }
+
+    public virtual bool TryReadCExoLocString(out string value, int id = 0, Gender gender = Gender.Male)
+    {
+      value = default;
+      return false;
+    }
+
+    public virtual bool TryReadChar(out byte value)
+    {
+      value = default;
+      return false;
+    }
+
+    public virtual bool TryReadWord(out ushort value)
+    {
+      value = default;
+      return false;
+    }
+
+    public virtual bool TryReadDWord(out uint value)
+    {
+      value = default;
+      return false;
+    }
+
+    public virtual bool TryReadDWord64(out ulong value)
+    {
+      value = default;
+      return false;
+    }
+
+    public virtual bool TryReadFloat(out float value)
+    {
+      value = default;
+      return false;
+    }
+
+    public virtual bool TryReadShort(out short value)
+    {
+      value = default;
+      return false;
+    }
+
+    public virtual bool TryReadDouble(out double value)
+    {
+      value = default;
+      return false;
     }
 
     public void Dispose()
--- src/main/Anvil/API/Resources/Gff/Fields/GffResourceFieldValue.cs
+++ src/main/Anvil/API/Resources/Gff/Fields/GffResourceFieldValue.cs
@@ -1,3 +1,4 @@
+using System;
 using NWN.Native.API;
 
 namespace Anvil.API
@@ -18,96 +19,91 @@ namespace Anvil.API
       get => (GffResourceFieldType)ResGff.GetFieldType(parentStruct, fieldId);
     }
 
-    protected override string ReadCExoString()
-    {
-      return TryReadCExoString(out string retVal) ? retVal : null;
-    }
-
-    public bool TryReadCExoString(out string value)
+    public override bool TryReadCExoString(out string value)
     {
       int bSuccess;
       value = ResGff.ReadFieldCExoString(parentStruct, fieldId, &bSuccess).ToString();
       return bSuccess.ToBool();
     }
 
-    public bool TryReadCResRef(out CResRef value)
+    public override bool TryReadCResRef(out string value)
     {
       int bSuccess;
-      value = ResGff.ReadFieldCResRef(parentStruct, fieldId, &bSuccess);
+      value = ResGff.ReadFieldCResRef(parentStruct, fieldId, &bSuccess).ToString();
       return bSuccess.ToBool();
     }
 
-    public bool TryReadInt(out int value)
+    public override bool TryReadInt(out int value)
     {
       int bSuccess;
       value = ResGff.ReadFieldINT(parentStruct, fieldId, &bSuccess);
       return bSuccess.ToBool();
     }
 
-    public bool TryReadInt64(out long value)
+    public override bool TryReadInt64(out long value)
     {
       int bSuccess;
       value = ResGff.ReadFieldINT64(parentStruct, fieldId, &bSuccess);
       return bSuccess.ToBool();
     }
 
-    public bool TryReadByte(out byte value)
+    public override bool TryReadByte(out byte value)
     {
       int bSuccess;
       value = ResGff.ReadFieldBYTE(parentStruct, fieldId, &bSuccess);
       return bSuccess.ToBool();
     }
 
-    public bool TryReadCExoLocString(out CExoLocString value)
+    public override bool TryReadCExoLocString(out string value, int id = 0, Gender gender = Gender.Male)
     {
       int bSuccess;
-      value = ResGff.ReadFieldCExoLocString(parentStruct, fieldId, &bSuccess);
+      value = ResGff.ReadFieldCExoLocString(parentStruct, fieldId, &bSuccess).ExtractLocString(id, (byte)gender);
       return bSuccess.ToBool();
     }
 
-    public bool TryReadChar(out byte value)
+    public override bool TryReadChar(out byte value)
     {
       int bSuccess;
       value = ResGff.ReadFieldCHAR(parentStruct, fieldId, &bSuccess);
       return bSuccess.ToBool();
     }
 
-    public bool TryReadWord(out ushort value)
+    public override bool TryReadWord(out ushort value)
     {
       int bSuccess;
       value = ResGff.ReadFieldWORD(parentStruct, fieldId, &bSuccess);
       return bSuccess.ToBool();
     }
 
-    public bool TryReadDWord(out uint value)
+    public override bool TryReadDWord(out uint value)
     {
       int bSuccess;
       value = ResGff.ReadFieldDWORD(parentStruct, fieldId, &bSuccess);
       return bSuccess.ToBool();
     }
 
-    public bool TryReadDWord64(out ulong value)
+    public override bool TryReadDWord64(out ulong value)
     {
       int bSuccess;
       value = ResGff.ReadFieldDWORD64(parentStruct, fieldId, &bSuccess);
       return bSuccess.ToBool();
     }
 
-    public bool TryReadFloat(out float value)
+    public override bool TryReadFloat(out float value)
     {
       int bSuccess;
       value = ResGff.ReadFieldFLOAT(parentStruct, fieldId, &bSuccess);
       return bSuccess.ToBool();
     }
 
-    public bool TryReadShort(out short value)
+    public override bool TryReadShort(out short value)
     {
       int bSuccess;
       value = ResGff.ReadFieldSHORT(parentStruct, fieldId, &bSuccess);
       return bSuccess.ToBool();
     }
 
-    public bool TryReadDouble(out double value)
+    public override bool TryReadDouble(out double value)
     {
       int bSuccess;
       value = ResGff.ReadFieldDOUBLE(parentStruct, fieldId, &bSuccess);
