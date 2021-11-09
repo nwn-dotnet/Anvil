aef8feffa9c9df9266c93cddf82a169cb61d0f67
Jorteck <jorteckdev@gmail.com>
2021-11-10 00:02:38 +0100

    Implement group set layout.

--- src/main/Anvil/API/Extensions/CollectionExtensions.cs
+++ src/main/Anvil/API/Extensions/CollectionExtensions.cs
@@ -92,6 +92,18 @@ namespace Anvil.API
       yield return item;
     }
 
+    /// <summary>
+    /// Wraps this object instance into an IEnumerable&lt;T&gt; consisting of a single item.<br/>
+    /// If the item is null, returns an empty enumerable instead.
+    /// </summary>
+    /// <typeparam name="T">Type of the object.</typeparam>
+    /// <param name="item">The instance that will be wrapped.</param>
+    /// <returns>An IEnumerable&lt;T&gt; consisting of a single item. </returns>
+    public static IEnumerable<T> SafeYield<T>(this T item)
+    {
+      return item != null ? item.Yield() : Enumerable.Empty<T>();
+    }
+
     public static void DisposeAll(this IEnumerable<IDisposable> disposables)
     {
       if (disposables == null)
