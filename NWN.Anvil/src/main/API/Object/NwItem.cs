131e40a16ec21034360826b33cad2057f157ff0b
urothis <natecaple@gmail.com>
2021-11-21 05:53:45 +0000

    Can Item Stack

--- NWN.Anvil/src/main/API/Object/NwItem.cs
+++ NWN.Anvil/src/main/API/Object/NwItem.cs
@@ -55,6 +55,15 @@ namespace Anvil.API
     }
 
     /// <summary>
+    /// Gets if this item can stack.
+    /// </summary>
+    public bool CanStack
+    {
+      get => int.Parse(NWScript.Get2DAString("baseitems","Stacking",((int)this.BaseItemType))) > 1;
+    }
+
+
+    /// <summary>
     /// Gets or sets the number of stacked items attached to this item.
     /// </summary>
     public int StackSize
