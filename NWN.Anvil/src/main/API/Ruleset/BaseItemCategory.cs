5d2eedf7991554aee463ddc4537d061257a3a62c
Jorteck <jorteckdev@gmail.com>
2021-12-16 15:52:55 +0100

    NwBaseItem: Implement native properties (#440)
    * Add properties to NwBaseItem.
    
    * Sort properties.
    
    * Update docs.
    
    * Update abbreviations list.
    
    * ModelType is byte enum.

---
+++ NWN.Anvil/src/main/API/Ruleset/BaseItemCategory.cs
@@ -0,0 +1,26 @@
+namespace Anvil.API
+{
+  public enum BaseItemCategory : byte
+  {
+    None = 0,
+    Melee = 1,
+    Ranged = 2,
+    Shield = 3,
+    Armor = 4,
+    Helmet = 5,
+    Ammo = 6,
+    Thrown = 7,
+    Staves = 8,
+    Potion = 9,
+    Scroll = 10,
+    ThievesTools = 11,
+    Misc = 12,
+    Wands = 13,
+    Rods = 14,
+    Traps = 15,
+    MiscUnequippable = 16,
+    Container = 17,
+    Healers = 19,
+    Torches = 20,
+  }
+}
---
+++ NWN.Anvil/src/main/API/Ruleset/BaseItemQBBehaviour.cs
@@ -0,0 +1,9 @@
+namespace Anvil.API
+{
+  public enum BaseItemQBBehaviour : byte
+  {
+    Default = 0,
+    SelectSpellNormal = 1,
+    SelectSpellTargetSelf = 2,
+  }
+}
