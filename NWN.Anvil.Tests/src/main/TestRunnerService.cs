7017417b5c4cfa5215f9f36eeb005b738da6a5d8
Jorteck <jorteckdev@gmail.com>
2021-12-26 04:08:50 +0100

    Implement Automatic Tests (#448)
    * Add NWN.Anvil.Tests project.
    
    * Add NUnit.
    
    * Implement custom test runner.
    
    * Shutdown server after test run.
    
    * Implement docker test run configuration.
    
    * Mount results volume.
    
    * Add color tests.
    
    * Implement test workflow.
    
    * Exclude lib folder from analysis.
    
    * Move test runner to lib folder.

---
+++ NWN.Anvil.Tests/src/main/TestRunnerService.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Linq;
+using Anvil.API;
+using Anvil.API.Events;
+using Anvil.Services;
+using Microsoft.CodeAnalysis;
+
+namespace Anvil.Tests
+{
+  [ServiceBinding(typeof(TestRunnerService))]
+  internal sealed class TestRunnerService
+  {
+    [Inject]
+    private PluginStorageService PluginStorageService { get; init; }
+
+    public TestRunnerService()
+    {
+      NwModule.Instance.OnModuleLoad += OnModuleLoad;
+    }
+
+    private void OnModuleLoad(ModuleEvents.OnModuleLoad eventData)
+    {
+      string[] args = GetRunnerArguments();
+      AnvilTestRunner testRunner = new AnvilTestRunner(typeof(TestRunnerService).Assembly);
+
+      testRunner.Execute(args);
+      NwServer.Instance.ShutdownServer();
+    }
+
+    private string[] GetRunnerArguments()
+    {
+      string outputPath = PluginStorageService.GetPluginStoragePath(typeof(TestRunnerService).Assembly);
+      string args = $"--work={outputPath}";
+      return string.IsNullOrEmpty(args) ? Array.Empty<string>() : CommandLineParser.SplitCommandLineIntoArguments(args, false).ToArray();
+    }
+  }
+}
