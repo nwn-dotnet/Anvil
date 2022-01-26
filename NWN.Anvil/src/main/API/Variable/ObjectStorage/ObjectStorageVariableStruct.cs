f650301592c6722e0b3cf6d60d0a752f4651bf2a
Jorteck <jorteckdev@gmail.com>
2022-01-26 22:17:51 +0100

    Add generic struct variables for objects.

---
+++ NWN.Anvil/src/main/API/Variable/ObjectStorage/ObjectStorageVariableStruct.cs
@@ -0,0 +1,44 @@
+using System.Text.Json;
+using Anvil.Services;
+
+namespace Anvil.API
+{
+  public abstract class ObjectStorageVariableStruct<T> : ObjectStorageVariable<T>
+  {
+    [Inject]
+    internal ObjectStorageService ObjectStorageService { private get; init; }
+
+    public sealed override bool HasValue => ObjectStorageService.TryGetObjectStorage(Object, out ObjectStorage objectStorage) && objectStorage.ContainsString(ObjectStoragePrefix, Key);
+
+    public sealed override T Value
+    {
+      get => JsonSerializer.Deserialize<T>(ObjectStorageService.GetObjectStorage(Object).GetString(ObjectStoragePrefix, Key));
+      set => ObjectStorageService.GetObjectStorage(Object).Set(ObjectStoragePrefix, Key, JsonSerializer.Serialize(value), Persist);
+    }
+
+    protected sealed override string VariableTypePrefix => "PERSTR!";
+
+    public sealed override void Delete()
+    {
+      ObjectStorageService.GetObjectStorage(Object).Remove(ObjectStoragePrefix, Key);
+    }
+  }
+
+  public sealed class PersistentVariableStruct<T> : ObjectStorageVariableStruct<T>
+  {
+    protected override string ObjectStoragePrefix => "NWNX_Object";
+    protected override bool Persist => true;
+  }
+
+  internal sealed class InternalVariableStruct<T> : ObjectStorageVariableStruct<T>
+  {
+    protected override string ObjectStoragePrefix => "ANVIL_API";
+    protected override bool Persist => true;
+
+    internal sealed class Persistent : ObjectStorageVariableString
+    {
+      protected override string ObjectStoragePrefix => "ANVIL_API";
+      protected override bool Persist => false;
+    }
+  }
+}
