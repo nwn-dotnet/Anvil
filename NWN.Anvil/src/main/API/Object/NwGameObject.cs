d7603a0cb9f16cec9af4c90832d0b48a33daa887
Jorteck <jorteckdev@gmail.com>
2022-09-01 00:50:35 +0200

    Use expression style property.

--- NWN.Anvil/src/main/API/Object/NwGameObject.cs
+++ NWN.Anvil/src/main/API/Object/NwGameObject.cs
@@ -48,10 +48,7 @@ namespace Anvil.API
     /// <summary>
     /// Gets the current animation state of this object.
     /// </summary>
-    public AnimationState AnimationState
-    {
-      get => (AnimationState)gameObject.m_nAnimation;
-    }
+    public AnimationState AnimationState => (AnimationState)gameObject.m_nAnimation;
 
     /// <summary>
     /// Gets the area this object is currently in.
