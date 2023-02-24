0c7d9c382ed146011ef835b8ca48a058762fcaed
Jhett Black <10942655+jhett12321@users.noreply.github.com>
2023-02-24 13:23:19 +1000

    Add sync context docs.

--- NWN.Anvil/src/main/API/Async/Awaiters/SynchronizationContextAwaiter.cs
+++ NWN.Anvil/src/main/API/Async/Awaiters/SynchronizationContextAwaiter.cs
@@ -2,6 +2,9 @@ using System.Threading;
 
 namespace Anvil.API
 {
+  /// <summary>
+  /// Awaits for the current async context to return to the specified context.
+  /// </summary>
   internal readonly struct SynchronizationContextAwaiter : IAwaiter
   {
     private static readonly SendOrPostCallback PostCallback = state => ((System.Action?)state)?.Invoke();
