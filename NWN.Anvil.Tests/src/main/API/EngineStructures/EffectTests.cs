5233653493958e3d9af78c50e8ffe62d70ac27c9
Jhett Black <10942655+jhett12321@users.noreply.github.com>
2023-02-24 15:07:15 +1000

    Add garbage collection test for effects.

--- NWN.Anvil.Tests/src/main/API/EngineStructures/EffectTests.cs
+++ NWN.Anvil.Tests/src/main/API/EngineStructures/EffectTests.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Runtime.CompilerServices;
 using Anvil.API;
 using NUnit.Framework;
+using NWN.Core;
 using NWN.Native.API;
 
 namespace Anvil.Tests.API
@@ -8,12 +11,35 @@ namespace Anvil.Tests.API
   public sealed class EffectTests
   {
     [Test(Description = "Creating an effect and disposing the effect explicitly frees the associated memory.")]
-    public void CreateAndDisposeEffectValidPropertyUpdated()
+    public void CreateAndDisposeEffectFreesNativeStructure()
     {
       Effect effect = Effect.CutsceneParalyze();
       Assert.That(effect.IsValid, Is.True, "Effect was not valid after creation.");
       effect.Dispose();
       Assert.That(effect.IsValid, Is.False, "Effect was still valid after disposing.");
+    }
+
+    [Test(Description = "Creating an effect and waiting for garbage collection frees the associated memory.")]
+    [Timeout(10000)]
+    public void CreateAndGarbageCollectEffectFreesNativeStructure()
+    {
+      GetWeakEffectReference(out IntPtr effectPtr, out WeakReference effectRef);
+
+      while (effectRef.IsAlive)
+      {
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+      }
+
+      Assert.That(NWScript.GetIsEffectValid(effectPtr).ToBool(), "Effect was still valid after garbage collection.");
+    }
+
+    [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+    private void GetWeakEffectReference(out IntPtr effectPtr, out WeakReference effectRef)
+    {
+      Effect effect = Effect.CutsceneParalyze();
+      effectPtr = new IntPtr((long)(IntPtr)effect);
+      effectRef = new WeakReference(effect);
     }
 
     [Test(Description = "A soft effect reference created from a native object does not cause the original effect to be deleted.")]
