1bbf99a3fd1409aef9382aefcc090a64168465c8
Jhett Black <10942655+jhett12321@users.noreply.github.com>
2023-03-04 19:56:26 +1000

    Implement native function address resolving.

--- NWN.Anvil/src/main/Services/Core/Hooking/NativeFunctionAttribute.cs
+++ NWN.Anvil/src/main/Services/Core/Hooking/NativeFunctionAttribute.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Runtime.InteropServices;
 
 namespace Anvil.Services
 {
   [AttributeUsage(AttributeTargets.Delegate)]
   public sealed class NativeFunctionAttribute : Attribute
   {
+    private static readonly IntPtr MainProgramHandle = NativeLibrary.GetMainProgramHandle();
+
     public string GccExportName { get; }
     public string MsvcExportName { get; }
     public IntPtr Address { get; }
@@ -14,19 +17,19 @@ namespace Anvil.Services
       GccExportName = gccExportName;
       MsvcExportName = msvcExportName;
 
-      // OperatingSystem os = Environment.OSVersion;
-      // switch (os.Platform)
-      // {
-      //   case PlatformID.Win32S:
-      //   case PlatformID.Win32Windows:
-      //   case PlatformID.Win32NT:
-      //   case PlatformID.WinCE:
-      //     Address = NativeLibrary.GetExport(NativeLibrary.GetMainProgramHandle(), msvcExportName);
-      //     break;
-      //   default:
-      //     Address = NativeLibrary.GetExport(NativeLibrary.GetMainProgramHandle(), gccExportName);
-      //     break;
-      // }
+      OperatingSystem os = Environment.OSVersion;
+      switch (os.Platform)
+      {
+        case PlatformID.Win32S:
+        case PlatformID.Win32Windows:
+        case PlatformID.Win32NT:
+        case PlatformID.WinCE:
+          Address = NativeLibrary.GetExport(MainProgramHandle, msvcExportName);
+          break;
+        default:
+          Address = NativeLibrary.GetExport(MainProgramHandle, gccExportName);
+          break;
+      }
     }
   }
 }
