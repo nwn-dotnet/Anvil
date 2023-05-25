6803d6049e8c777ec210e547588df54eb2e0b99d
Jhett Black <10942655+jhett12321@users.noreply.github.com>
2023-05-25 03:13:56 +0200

    Add web request tests.

---
+++ NWN.Anvil.Tests/src/main/System/WebRequestTests.cs
@@ -0,0 +1,20 @@
+using System.Net.Http;
+using NUnit.Framework;
+
+namespace Anvil.Tests.System
+{
+  [TestFixture(Category = "TestRunner")]
+  public sealed class WebRequestTests
+  {
+    [Test]
+    [TestCase("http://example.com")]
+    [TestCase("https://example.com")]
+    public void InvokeWebRequestIsSuccessful(string uri)
+    {
+      HttpClient httpClient = new HttpClient();
+      HttpResponseMessage response = httpClient.Send(new HttpRequestMessage(HttpMethod.Get, uri));
+
+      Assert.That(response.IsSuccessStatusCode, Is.True, $"'{uri}' return a non-success code: {response.StatusCode}");
+    }
+  }
+}
