29157b2dec14982ec3ae45644eec425784d592d9
Jhett Black <10942655+jhett12321@users.noreply.github.com>
2023-09-01 22:48:14 +0200

    AddPropPolicy docs.

--- NWN.Anvil/src/main/API/Constants/AddPropPolicy.cs
+++ NWN.Anvil/src/main/API/Constants/AddPropPolicy.cs
@@ -2,8 +2,19 @@ namespace Anvil.API
 {
   public enum AddPropPolicy
   {
-    ReplaceExisting = 0,
-    KeepExisting = 1,
-    IgnoreExisting = 2,
+    /// <summary>
+    /// Always add the property, regardless of if a matching one already exists.
+    /// </summary>
+    IgnoreExisting,
+
+    /// <summary>
+    /// Remove and replace matching existing properties with the new property.
+    /// </summary>
+    ReplaceExisting,
+
+    /// <summary>
+    /// Keep existing matching properties, don't add the new property if it already exists.
+    /// </summary>
+    KeepExisting,
   }
 }
