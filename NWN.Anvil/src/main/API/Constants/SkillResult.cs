b14b99d265e9c652e58ea6cceda32734e9a4cd33
Jhett Black <10942655+jhett12321@users.noreply.github.com>
2023-09-22 00:46:08 +0200

    NwCreature: Add BroadcastSkillRoll. (#719)

---
+++ NWN.Anvil/src/main/API/Constants/SkillResult.cs
@@ -0,0 +1,12 @@
+namespace Anvil.API
+{
+  public enum SkillResult
+  {
+    Failure = 0,
+    Success = 1,
+    CriticalFailure = 2,
+    SuccessNotPossible = 3,
+    AutomaticSuccess = 4,
+    SuccessNeverPossible = 5,
+  }
+}
