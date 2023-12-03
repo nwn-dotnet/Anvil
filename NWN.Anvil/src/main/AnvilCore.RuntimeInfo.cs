763314ef15a0ed7488e92789c1712bb199f20617
Jhett Black <10942655+jhett12321@users.noreply.github.com>
2023-12-03 21:17:44 +0100

    Resolve runtime info at startup. Use console instead of nlog for crash message.

---
+++ NWN.Anvil/src/main/AnvilCore.RuntimeInfo.cs
@@ -0,0 +1,19 @@
+using Anvil.API;
+using Anvil.Internal;
+
+namespace Anvil
+{
+  public sealed partial class AnvilCore
+  {
+    private static RuntimeInfo runtimeInfo;
+
+    private struct RuntimeInfo
+    {
+      public string? AssemblyName;
+      public string? AssemblyVersion;
+      public string? ServerVersion;
+      public string? CoreVersion;
+      public string? NativeVersion;
+    }
+  }
+}
