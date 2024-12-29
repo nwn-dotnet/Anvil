d5d4409843a246be8b20418a624ef92e7d86a39e
Jhett Black <10942655+jhett12321@users.noreply.github.com>
2024-12-29 12:24:40 +0100

    Empty declaration body.

--- NWN.Anvil/src/main/API/Nui/Widgets/NuiWidget.cs
+++ NWN.Anvil/src/main/API/Nui/Widgets/NuiWidget.cs
@@ -3,5 +3,5 @@ namespace Anvil.API
   /// <summary>
   /// The abstract base for all NUI widgets - the building blocks for creating NUI windows.
   /// </summary>
-  public abstract class NuiWidget : NuiElement {}
+  public abstract class NuiWidget : NuiElement;
 }
