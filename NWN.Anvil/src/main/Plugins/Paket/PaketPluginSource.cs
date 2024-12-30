e5d4adae262550aec7658a56e4582489b43d92b9
Jhett Black <10942655+jhett12321@users.noreply.github.com>
2024-12-30 18:41:03 +0100

    Cleanup import.

--- NWN.Anvil/src/main/Plugins/Paket/PaketPluginSource.cs
+++ NWN.Anvil/src/main/Plugins/Paket/PaketPluginSource.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using Anvil.Internal;
 using Anvil.Services;
 using Microsoft.FSharp.Collections;
