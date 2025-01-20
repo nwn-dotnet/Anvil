1271b6b8567bd5798501158f1ba66439f0b5dfd7
Jhett Black <10942655+jhett12321@users.noreply.github.com>
2025-01-20 23:07:24 +0100

    Update constants.

---
+++ NWN.Anvil/src/main/API/Constants/SpellFailureType.cs
@@ -0,0 +1,10 @@
+﻿using NWN.Core;
+
+namespace Anvil.API
+{
+  public enum SpellFailureType
+  {
+    All = NWScript.SPELL_FAILURE_TYPE_ALL,
+    Arcane = NWScript.SPELL_FAILURE_TYPE_ARCANE,
+  }
+}
