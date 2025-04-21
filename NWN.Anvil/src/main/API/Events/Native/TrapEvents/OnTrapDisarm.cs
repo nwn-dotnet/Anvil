cf52e9b1981af7a98221dd6bfb2d43e3ee4be638
Jhett Black <10942655+jhett12321@users.noreply.github.com>
2025-04-21 16:44:27 +0200

    Add trap events.

---
+++ NWN.Anvil/src/main/API/Events/Native/TrapEvents/OnTrapDisarm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Anvil.API.Events;
+using Anvil.Native;
+using Anvil.Services;
+
+namespace Anvil.API.Events
+{
+  /// <summary>
+  /// Called when a creature attempts to disarm a trap.
+  /// </summary>
+  public sealed class OnTrapDisarm : TrapEvent
+  {
+    public sealed unsafe class Factory : TrapHookEventFactory
+    {
+      private static FunctionHook<Functions.CNWSCreature.AIActionDisarmTrap> Hook { get; set; } = null!;
+
+      protected override IDisposable[] RequestHooks()
+      {
+        delegate* unmanaged<void*, void*, uint> pHook = &OnAIActionDisarmTrap;
+        Hook = HookService.RequestHook<Functions.CNWSCreature.AIActionDisarmTrap>(pHook, HookOrder.Early);
+        return [Hook];
+      }
+
+      [UnmanagedCallersOnly]
+      private static uint OnAIActionDisarmTrap(void* pCreature, void* pNode)
+      {
+        OnTrapDisarm eventData = HandleExistingTrapEvent<OnTrapDisarm>(pCreature, pNode);
+        if (eventData.ResultOverride is not (ActionState.Complete or ActionState.Failed))
+        {
+          return Hook.CallOriginal(pCreature, pNode);
+        }
+
+        eventData.Result = eventData.ResultOverride.Value;
+        ProcessEvent(EventCallbackType.After, eventData);
+
+        return (uint)eventData.Result;
+      }
+    }
+  }
+}
+
+namespace Anvil.API
+{
+  public sealed partial class NwCreature
+  {
+    /// <inheritdoc cref="Events.OnTrapDisarm"/>
+    public event Action<OnTrapDisarm> OnTrapDisarm
+    {
+      add => EventService.Subscribe<OnTrapDisarm, OnTrapDisarm.Factory>(this, value);
+      remove => EventService.Unsubscribe<OnTrapDisarm, OnTrapDisarm.Factory>(this, value);
+    }
+  }
+
+  public sealed partial class NwModule
+  {
+    /// <inheritdoc cref="Events.OnTrapDisarm"/>
+    public event Action<OnTrapDisarm> OnTrapDisarm
+    {
+      add => EventService.SubscribeAll<OnTrapDisarm, OnTrapDisarm.Factory>(value);
+      remove => EventService.UnsubscribeAll<OnTrapDisarm, OnTrapDisarm.Factory>(value);
+    }
+  }
+}
---
+++ NWN.Anvil/src/main/API/Events/Native/TrapEvents/TrapEvent.cs
@@ -0,0 +1,33 @@
+﻿namespace Anvil.API.Events
+{
+  public abstract class TrapEvent : IEvent
+  {
+    /// <summary>
+    /// Gets if the creature is in range of the trap.
+    /// </summary>
+    public bool InRange { get; internal init; }
+
+    /// <summary>
+    /// Gets the creature performing the trap action.
+    /// </summary>
+    public NwCreature Creature { get; internal init; } = null!;
+
+    /// <summary>
+    /// Gets the target trap for this trap event.
+    /// </summary>
+    public NwGameObject Trap { get; internal init; } = null!;
+
+    /// <summary>
+    /// Gets or sets a value to override the trap action result, skipping the default game behaviour.<br/>
+    /// Supported values: <see cref="ActionState.Complete"/>, <see cref="ActionState.Failed"/>.
+    /// </summary>
+    public ActionState? ResultOverride { get; set; }
+
+    /// <summary>
+    /// Gets the result of this trap event. This value is only valid in the after event.
+    /// </summary>
+    public ActionState Result { get; internal set; }
+
+    NwObject IEvent.Context => Creature;
+  }
+}
