fef6c9ea0ad12a19f6283a82d0f201d01d5f88a4
Jhett Black <10942655+jhett12321@users.noreply.github.com>
2026-08-22 12:23:56 +0200

    Use implicit conversion.

--- NWN.Anvil/src/main/Services/Core/Logging/LoggerManager.cs
+++ NWN.Anvil/src/main/Services/Core/Logging/LoggerManager.cs
@@ -65,7 +65,7 @@ namespace Anvil.Services
       FileTarget fileTarget = new FileTarget("anvil.log")
       {
         Layout = DefaultLayout,
-        FileName = new SimpleLayout("${var:nwn_home}/logs.0/anvil.log"),
+        FileName = "${var:nwn_home}/logs.0/anvil.log",
         CreateDirs = false,
         KeepFileOpen = true,
         OpenFileCacheTimeout = 30,
