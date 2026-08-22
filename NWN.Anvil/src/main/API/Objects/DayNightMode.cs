6ac9e88d6c11b4b5ee0d1a0de5fca6ee56192e99
Jhett Black <10942655+jhett12321@users.noreply.github.com>
2026-08-22 23:49:02 +0200

    Lint cleanup.

--- NWN.Anvil/src/main/API/Objects/DayNightMode.cs
+++ NWN.Anvil/src/main/API/Objects/DayNightMode.cs
@@ -9,10 +9,12 @@ namespace Anvil.API
     /// Time progresses normally and the area transitions between day and night.
     /// </summary>
     EnableDayNightCycle,
+
     /// <summary>
     /// The area is fixed to night visuals; transitions do not occur.
     /// </summary>
     AlwaysNight,
+
     /// <summary>
     /// The area is fixed to day visuals; transitions do not occur.
     /// </summary>
